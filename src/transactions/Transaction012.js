9d57d785e72fb06898894a263595d0f960b57950
jv88899 <jv88899@gmail.com>
2020-12-07 10:19:53 -0600

    added click handler to 012

--- src/transactions/Transaction012.js
+++ src/transactions/Transaction012.js
@@ -148,8 +148,17 @@ const Transaction012 = ({ companyName }) => (
                       <span className="item-total">11</span>
                     </li>
                     <li>
-                      <span className="item-name blue">IBNR</span>
-                      <span className="item-total">0</span>
+                      <Link
+                        to="013"
+                        style={{
+                          color: "inherit",
+                          cursor: "default",
+                          textDecoration: "none"
+                        }}
+                      >
+                        <span className="item-name blue">IBNR</span>
+                        <span className="item-total">0</span>
+                      </Link>
                     </li>
                     <li>
                       <span className="item-name blue">Salary Payable</span>
