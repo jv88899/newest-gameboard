d07831b4c49b231777916d1e0fbb72e65b32fb72
jv88899 <jv88899@gmail.com>
2020-12-07 15:41:48 -0600

    added router to 041

--- src/transactions/Transaction041.js
+++ src/transactions/Transaction041.js
@@ -218,8 +218,17 @@ const Transaction041 = ({ companyName }) => (
               <span className="item-total">1</span>
             </li>
             <li>
-              <span className="item-name red">Taxing Authority</span>
-              <span className="item-total">0</span>
+              <Link
+                to="/042"
+                style={{
+                  color: "inherit",
+                  cursor: "default",
+                  textDecoration: "none"
+                }}
+              >
+                <span className="item-name red">Taxing Authority</span>
+                <span className="item-total">0</span>
+              </Link>
             </li>
           </ul>
         </div>
