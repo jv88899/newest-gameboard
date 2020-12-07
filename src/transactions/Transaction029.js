f9387ce2685dcf8b3478f926431caa6cc807a802
jv88899 <jv88899@gmail.com>
2020-12-07 13:44:53 -0600

    added click handler to 030

--- src/transactions/Transaction029.js
+++ src/transactions/Transaction029.js
@@ -132,10 +132,19 @@ const Transaction029 = ({ companyName }) => (
                       <span className="item-total">0</span>
                     </li>
                     <li>
-                      <span className="item-name blue">
-                        Property & Equipment
-                      </span>
-                      <span className="item-total">0</span>
+                      <Link
+                        to="/030"
+                        style={{
+                          color: "inherit",
+                          cursor: "default",
+                          textDecoration: "none"
+                        }}
+                      >
+                        <span className="item-name blue">
+                          Property & Equipment
+                        </span>
+                        <span className="item-total">0</span>
+                      </Link>
                     </li>
                   </ul>
                 </div>
