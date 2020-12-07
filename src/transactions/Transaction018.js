4c2ed9fe26908c36c9d4332a1c9cbd2de4bc71fd
jv88899 <jv88899@gmail.com>
2020-12-07 10:44:46 -0600

    added click handler to 018

--- src/transactions/Transaction018.js
+++ src/transactions/Transaction018.js
@@ -152,8 +152,17 @@ const Transaction018 = ({ companyName }) => (
                       <span className="item-total">2</span>
                     </li>
                     <li>
-                      <span className="item-name blue">Salary Payable</span>
-                      <span className="item-total">1</span>
+                      <Link
+                        to="/019"
+                        style={{
+                          color: "inherit",
+                          cursor: "default",
+                          textDecoration: "none"
+                        }}
+                      >
+                        <span className="item-name blue">Salary Payable</span>
+                        <span className="item-total">1</span>
+                      </Link>
                     </li>
                     <li>
                       <span className="item-name blue">Broker Payable</span>
