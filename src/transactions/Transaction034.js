7a2a0aed0e752f4e38416f0e8d68015b81d8f9a6
jv88899 <jv88899@gmail.com>
2020-12-07 14:30:56 -0600

    added click handler to 034

--- src/transactions/Transaction034.js
+++ src/transactions/Transaction034.js
@@ -231,8 +231,17 @@ const Transaction034 = ({ companyName }) => (
               <span className="item-total">5</span>
             </li>
             <li>
-              <span className="item-name red">US Bank</span>
-              <span className="item-total">0</span>
+              <Link
+                to="/035"
+                style={{
+                  color: "inherit",
+                  cursor: "default",
+                  textDecoration: "none"
+                }}
+              >
+                <span className="item-name red">US Bank</span>
+                <span className="item-total">0</span>
+              </Link>
             </li>
           </ul>
         </div>
